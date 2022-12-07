452ab6b472a5e2bbcbb66912bf2599269741a598
Divya-1905 <divyasri1905@gmail.com>
2022-12-07 16:41:26 +0530

    models

--- core/models.py
+++ core/models.py
@@ -4,37 +4,34 @@ from bson import ObjectId
 import json
 import datetime
 import uuid
-from mongoengine import Document, StringField, EmailField, IntField, ObjectIdField, ValidationError, ReferenceField, CASCADE, BooleanField
+from mongoengine import Document, StringField, EmailField, IntField, ObjectIdField, ValidationError, ReferenceField, CASCADE, BooleanField,DateField
 from flask_login import UserMixin
-
 class User(Document, UserMixin):
     id = IntField(required=True, primary_key=True)
     email = EmailField(required=True)
     phone = IntField(required=True)
-    registernumber = StringField(required=True)
-    user_type = StringField(
-        choice={'is_student', 'is_staff', 'is_admin'}, default=None)
+    registernumber=StringField(required=True)
     is_active = BooleanField(default=True)
-
     meta = {'collections': 'user'}
 
-    def _init__(self, email, phone, registernumber):
+    def _init__(self, email, phone,registernumber):
         self.email = email
         self.phone = phone
-        self.registernumber = registernumber
+        self.registernumber=registernumber
 
     def get_id(self):
         return self.id
 
     def clean(self):
         users = User.objects
+        # if users(email__exists=self.email):
+        #     raise ValidationError({'error': 'altready exists'})
         if users:
-            self.id = (users[users.count()-1]).id + 1
+            self.id = users.count()
+            # this clean function is rewrite the object id startr with 0
         else:
             self.id = 0
-
-    def __repr__(self):
-        return self.email
+        print(users)
 
 
 class Token(Document):
@@ -49,25 +46,21 @@ class Token(Document):
     def save(self, *args, **kwargs):
         self.token_id = str(uuid.uuid4())
         super().save(*args, **kwargs)
-
-
 class Profile(Document):
-    _id = IntField(primary_key=True, required=True)
+    _id=IntField(primary_key=True,required=True)
     firstname = StringField(max_length=50)
     lastname = StringField(max_length=50)
-    fullname = StringField(max_length=50)
+    fullname= StringField(max_length=50)
     usertype = StringField(max_length=10)
     address = StringField(max_length=100)
-    user = ReferenceField(User, reverse_delete_rule=CASCADE)
-
-    def _init__(self, firstname, fullname, lastname, usertype, address, user):
-        self.fullname = fullname
-        self.lastname = lastname
-        self.usertype = usertype
-        self.address = address
-        self.firstname = firstname
-    meta = {'collection': 'profile'}
-
+    user=ReferenceField(User,reverse_delete_rule=CASCADE)
+    def _init__(self,firstname,fullname,lastname,usertype,address,user):
+      self.fullname=fullname
+      self.lastname=lastname
+      self.usertype=usertype
+      self.address=address
+      self.firstname = firstname
+    meta={'collection': 'profile'}
     def clean(self):
         users = Profile.objects
         # if users(email__exists=self.email):
@@ -77,6 +70,36 @@ class Profile(Document):
         else:
             self.id = 0
         print(users)
+class Grade(Document):
+    _id = IntField(primarykey=True,requried = True)
+    grade =IntField()
+    section = StringField(max_length=2)
+    def _init_(self,grade,section):
+        self.grade = grade
+        self.section = section
+    meta={'collection':'grade'} 
+    def clean(self):
+        grades = Grade.objects
+        if grades :
+            self.id = grades.count()
+        else:
+            self.id = 0
 
-    def __repr__(self):
-        return self.fullname
+class subject(Document):
+    name = StringField(max_length=20)
+    code = IntField()
+    grade = ReferenceField(Grade,reverse_delete_rule=CASCADE)    
+    created_at = DateField(default=datetime)
+    def _init_(self,name,code,grade,created_at):
+        self.name = name
+        self.code = code
+        self.created_at = created_at
+    meta={'collection':'subject'}
+    def clean(self):
+        subject = subject.objects
+        if subject:
+            self.id = subject.count()
+        else:
+            self.id=0 
+
+
