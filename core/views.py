d7440c984f797da867feb48504b2eca7a8e6c8c8
Divya-1905 <divyasri1905@gmail.com>
2022-12-07 16:44:07 +0530

    models

--- core/views.py
+++ core/views.py
@@ -1,48 +1,26 @@
-from .models import User, Profile, Token
-from mongoengine import connect, get_db
-from bson.json_util import dumps
-from . serializer import UserSerializer
+from .models import User,Profile,Token,Grade,subject
 import uuid
-from .extension import mongo
-from .models import User, Token
-from flask import Blueprint, render_template, request, jsonify, make_response, Response
-from functools import wraps
-from flask import session
+from . serializer import UserSerializer
+from mongoengine import connect,get_db
+connect(host='mongodb+srv://root:12345@cluster0.tnb7vm7.mongodb.net/school')
+db=get_db()
+from flask import Blueprint, render_template, request,Response
+from bson.json_util import dumps
+from flask_login import login_user
 views = Blueprint('views', __name__)
-connect(
-    host='mongodb+srv://root:12345@cluster0.kv3gwol.mongodb.net/school'
-)
-db = get_db()
-
-
-def token_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session['token']:
-            token_id = session['token']
-            print(token_id)
-        else:
-            token_id = ((request.headers['Authorization']).split(' '))[-1]
-        token = Token.objects(token_id=token_id)
-        if not token:
-            return {'data': 'invalid token'}
-        user = token[0].user_id
-        return f(*args, **kwargs)
-    return decorated_function
 
 
 @views.route('/')
-@token_required
 def userdetails():
-    pipeline = [{"$lookup": {
-        "from": "profile",
-        "localField": "_id",
-        "foreignField": 'user',
-        "as": 'profile'
-    }}]
-    user = User.objects.aggregate(pipeline=pipeline)
-    query = list(user)
-    data = dumps(query)
+    pipeline=[{"$lookup":{
+  "from": "profile",
+  "localField": "_id",
+  "foreignField": 'user',
+  "as": 'profile'
+}}]
+    user=db.user.aggregate(pipeline=pipeline)
+    query=list(user)
+    data=dumps(query)
     return data
 
 
@@ -51,60 +29,43 @@ def login():
     data = request.get_json()
     user = User.objects(email=data['email'], phone=data['phone']).first()
     token = (Token.objects(user_id=user)).first()
-    session["token"] = token.token_id
-    print(session['token'])
+    login_user(user=user, remember=True)
     if not token:
         token = Token(user_id=user)
         token.save()
-    print(request.headers)
-    return {'status': user.to_json(), 'token': token.token_id}
-
-
-@views.route('/')
-@token_required
-def index():
-    return 'Hello, world'
-
+    return {'status': user.to_json(),'token':token.token_id}
 
 @views.route('signup/', methods=['POST'])
 def signup():
     try:
         print(request.json)
         data= request.json
-    
         user=User(email=data['email'],phone=data['phone'],registernumber=data['registernumber'])
         user.save()
-        profile=Profile(fullname=data['fullname'],firstname=data["firstname"],lastname=data['lastname'],address=data['address'],usertype=data['usertype'],user=user)
+        profile=Profile(fullname=data['fullname'],firstname=data["firstname"],
+        lastname=data['lastname'],address=data['address'],usertype=data['usertype'],user=user)
         profile.save()
         return Response(dumps({'message':'created'}),status=200)
     except Exception as e:
         print(e)
         return Response(dumps({'message':'not created'}),status=400)
-@views.route('user/<id>',methods=['GET','PATCH'])
+@views.route('user/<id>',methods=['GET'])
 def user(id):
-    pipeline = [{'$match': {'_id': int(id)}}, {"$lookup": {
-        "from": "profile",
-        "localField": "_id",
-        "foreignField": 'user',
-        "as": 'profile'
-    }}]
-    if request.method != 'PATCH':
-        try:
-            user=list(User.objects.aggregate(pipeline=pipeline))
-            data=dumps(user)
-            if user :
-                return Response(dumps({'data':data}),status=200)
-            return Response(dumps({'message':'User doesnt Existed'}),status=400)
-        except Exception as e:
-            print(e)
-            return Response(dumps({'message': e}), status=400)
-    try:
-        user=User.objects(id=int(id))
-        data=request.json
+    user=User.objects.get(pk=id)
+    return user.to_json()
+@views.route('grade/',methods =['POST'])
+def sub():
+    # try:
+        data = request.json
         print(data)
-        user.update(email=data['email'],phone=data['phone'],registernumber=data['registernumber'])
-        profile=Profile.objects(user=int(id))
-        profile.update(fullname=data['fullname'],firstname=data["firstname"],lastname=data['lastname'],address=data['address'],usertype=data['usertype'])
-        return Response(dumps({'data':'Working with patch method'}),status=200)
-    except Exception as e:
-        return Response(dumps({'message': e}), status=400)
+        grade = Grade(grade='1',section='c')
+        print(grade)
+        grade.save()
+        Subject = subject(name=data['name'],code=data['code'],created_at=data['created_at'],grade=grade)
+        print(Subject)
+        Subject.save()
+        return Response(dumps({'message':'successfull'}),status=200)
+    # except Exception as e :
+    #     return Response(dumps({"message":'unsuccessfull'}),status=404)    
+
+    
