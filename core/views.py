4b7707037fe761e495e93dce948e2033645918f1
siva1002 <sivasurlip@venzotechnologies.com>
2022-12-07 10:48:41 +0530

    new

--- core/views.py
+++ core/views.py
@@ -4,14 +4,14 @@ from . serializer import UserSerializer
 from mongoengine import connect,get_db
 connect(host='mongodb+srv://root:12345@cluster0.kv3gwol.mongodb.net/school')
 db=get_db()
-from flask import Blueprint, render_template, request, jsonify, make_response,Response
+from flask import Blueprint, render_template, request,Response
 from bson.json_util import dumps
 from flask_login import login_user
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
-def index():
+def userdetails():
     pipeline=[{"$lookup":{
   "from": "profile",
   "localField": "_id",
@@ -21,7 +21,6 @@ def index():
     user=db.user.aggregate(pipeline=pipeline)
     query=list(user)
     data=dumps(query)
-    print(data)
     return data
 
 
@@ -49,3 +48,7 @@ def signup():
     except Exception as e:
         print(e)
         return Response(dumps({'message':'not created'}),status=400)
+@views.route('user/<id>',methods=['GET'])
+def user(id):
+    user=User.objects.get(pk=id)
+    return user.to_json()
