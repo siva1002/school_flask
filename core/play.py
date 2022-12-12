2f931444d18741f8c28b292e80c12f742bc75f4e
Divya-1905 <divyasri1905@gmail.com>
2022-12-12 15:34:02 +0530

    update

--- core/play.py
+++
@@ -1,9 +0,0 @@
-class players:
-    def __init__(self,name,age):
-        self.name = name #attributies
-        self.age = age
-player1 = players('critiyana',6)
-print(player1.name) 
-print(player1.age)  
-
-
